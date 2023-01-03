7f86a00d4030cab1375d3d5c009c125b3622022c
yujin <miru93198@gmail.com>
2023-01-03 17:31:42 +0900

    button, header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import './App.css';
 import { BrowserRouter, Route, Routes} from 'react-router-dom';
 
-import RouteTest from './components/RouteTest';
 
 import Home from './pages/Home';
 import New from './pages/New';
 import Diary from './pages/Diary';
 import Edit from './pages/Edit';
 
+//COMPONENTS
+import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
+
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
+      <MyHeader headText={"App"} />
       <h2>App.js</h2>
+
+
+      <MyButton 
+        text={"버튼"}
+        onClick={()=> alert("버튼 클릭")}
+        type={"positive"}
+      />
+        <MyButton 
+        text={"버튼"}
+        onClick={()=> alert("버튼 클릭")}
+        type={"negative"}
+      />
+        <MyButton 
+        text={"버튼"}
+        onClick={()=> alert("버튼 클릭")}
+        
+      />
+      
+
       <Routes>
         <Route path='/' element = {<Home />} />
         <Route path='/new' element = {<New />} />
         <Route path='/edit' element = {<Edit />} />
         <Route path='/diary/:id' element = {<Diary />} />
       </Routes>
-      <RouteTest />
+      
     </div>
     </BrowserRouter>
   );
---
+++ src/components/MyHeader.js
@@ -0,0 +1,17 @@
+const MyHeader = ({leftChild, headText, rightChild}) => {
+    return(
+        <header>
+            <div className="head_btn_left">
+                {leftChild}
+            </div>
+            <div className="head_text">
+                {headText}
+            </div>
+            <div className="head_btn_left">
+                {rightChild}
+            </div>
+        </header>
+    );
+};
+
+export default MyHeader;
