6e568be53d04c2bed8849535131d25b3fc2c85eb
yujin <miru93198@gmail.com>
2023-01-11 17:41:16 +0900

    diaryList sort

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -16,17 +16,27 @@ const ControlMenu = ({value,onChange,optionList}) =>{
 };
 
 
-const DiaryList = ({diaryList}) => {
+const DiaryList = ({ diaryList }) => {
     const [sortType, setSortType] = useState("lastest"); //최신순
-
+    
     const getProcessedDiaryList = () =>{
+        const compare = (a,b) => {
+                if (sortType === 'latest'){
+                    return parseInt(b.date) - parseInt(a.date); //parseInt를 하는 이유 -> 문자열이 들어올 수도 있기 때문
+                }else{
+                    return parseInt(a.date) - parseInt(b.date);
+                }
+            
+            }
         const copyList = JSON.parse(JSON.stringify(diaryList)); //바로 sort를 쓰면 원본 배열 자체가 정렬됨.
         //diaryList라는 배열을 문자열로 만들어서 parse를 시키면 배열로 다시 만들어줌
+        const sortedList = copyList.sort(compare);
+        return sortedList;
     }
     return(
         <div>
             <ControlMenu value={sortType} onChange={setSortType} optionList={sortOptionList}/>
-            {diaryList.map((it)=> (
+            {getProcessedDiaryList.map((it)=> (
                 <div key={it.id}>
                     {it.content}
                 </div>
