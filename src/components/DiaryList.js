e2b1c138dc4e10e52a76cb57467eba52f38dcffb
yujin <miru93198@gmail.com>
2023-01-14 02:30:56 +0900

    filter sort & new navigate button

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,13 +1,21 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import MyButton from './MyButton';
 
 const sortOptionList = [
     {value: "latest", name:"최신순"},
     {value: "oldest", name:"오래된 순"},
 ];
 
+const filterOptionList = [
+    {value: "all", name: "전부 다"},
+    {value: "good", name: "좋은 감정만"},
+    {value: "bad", name: "안좋은 감정만"},
+]
+
 const ControlMenu = ({value, onChange, optionList}) => {
     return (
-        <select value={value} onChange={(e) => onChange(e.target.value)}>
+        <select className="ControlMenu" value={value} onChange={(e) => onChange(e.target.value)}>
             {optionList.map((it, idx) => (
                 <option key={idx} value={it.value}>
                     {it.name}
@@ -19,9 +27,21 @@ const ControlMenu = ({value, onChange, optionList}) => {
 //value -> select의 선택값, onChange -> select가 선택할 때 바꿀 함수, optionList -> 안에 옵션
 
 const DiaryList = ({ diaryList }) => {
-    const [sortType, setSortType] = useState("latest");
+    const navigate = useNavigate();
+    const [sortType, setSortType] = useState("lastest");
+    const [filter, setFilter] = useState("all");
 
 const getProcessedDiaryList = () => {
+
+    const filterCallBack = (item) =>{
+        if(filter === "good") {
+            return parseInt(item.emotion) <= 3;
+        }else{
+            return parseInt(item.emotion) > 3;
+        }
+
+    }
+
 
     const compare = (a,b) => {
         if(sortType === "latest"){
@@ -32,7 +52,9 @@ const getProcessedDiaryList = () => {
     }
 
     const copyList = JSON.parse(JSON.stringify(diaryList));// diaryList를 문자화 -> parse를 시키면 다시 배열로 돌아와
-    const sortedList = copyList.sort(compare); //바로 sort하면 원본 배열이 망가지니 copy본에 sort
+
+    const filteredList = filter === 'all' ? copyList : copyList.filter((it) => filterCallBack(it));
+    const sortedList = filteredList.sort(compare); //바로 sort하면 원본 배열이 망가지니 copy본에 sort
 
     return sortedList;
 } 
@@ -40,15 +62,33 @@ const getProcessedDiaryList = () => {
 
 
     return(
-        <div>
-            <ControlMenu 
-            value={sortType} 
-            onChange={setSortType}
-            optionList={sortOptionList}
-            />
+        <div className="DiaryList">
+            <div className="menu_wrapper">
+                <div className="left_col">
+                    <ControlMenu 
+                    value={sortType} 
+                    onChange={setSortType}
+                    optionList={sortOptionList}
+                    />
+                    <ControlMenu 
+                    value={filter}
+                    onChange={setFilter}
+                    optionList={filterOptionList}
+                    />
+                </div>
+                <div className="right_col">
+                    <MyButton 
+                        type={'positive'} 
+                        text="새 일기 쓰기" 
+                        onClick={ () => navigate("/new")}
+                        />
+                </div>
+            </div>
+            
+            
             {getProcessedDiaryList().map((it)=> (
                 <div key={it.id}>
-                    {it.content}
+                    {it.content} {it.emotion}
                 </div>
             ))}
         </div>
