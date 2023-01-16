6da5a1524554868c22d553fad8753c0f74b6e9f6
yujin <miru93198@gmail.com>
2023-01-16 17:36:43 +0900

    diaryItem 수정

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,18 +1,31 @@
+import MyButton from "./MyButton";
 
-const DairyItem = (id, emotion, content, date) => {
+const DairyItem = ({ id, emotion, content, date }) => {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
 
-    const env = process.env;
-    env.PUBLIC_URL = env.PUBLIC_URL || "";
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
 
-    return (
-    <div className="DairyItem">
-        <div className="emotion_img_wrapper">
-            <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+  return (
+    <div className="DiaryItem">
+      <div
+        className={["emotion_img_wrapper", `/assets/emotion${emotion}`].join(
+          " "
+        )}
+      >
+        <img src={process.env.PUBLIC_URL + `/assets/emotion${emotion}.png`} />
+      </div>
+      <div className="info_wrapper">
+        <div className="diary_date">{strDate}</div>
+        <div className="diary_content_preview">
+          {content.slice(0, 25)}
+          {/* slice해서 25글자까지만 보이게 하기 */}
         </div>
-        <div></div>
-        <div></div>
+      </div>
+      <div className="btn_wrapper">
+        <MyButton text={"수정하기"} />
+      </div>
     </div>
-    )
-    
-}
+  );
+};
 export default DairyItem;
--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,97 +1,98 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import MyButton from './MyButton';
-import DairyItem from './DiaryItem';
+import MyButton from "./MyButton";
+import DairyItem from "./DiaryItem";
 
 const sortOptionList = [
-    {value: "latest", name:"최신순"},
-    {value: "oldest", name:"오래된 순"},
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
 ];
 
 const filterOptionList = [
-    {value: "all", name: "전부 다"},
-    {value: "good", name: "좋은 감정만"},
-    {value: "bad", name: "안좋은 감정만"},
-]
+  { value: "all", name: "전부 다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안좋은 감정만" },
+];
 
-const ControlMenu = ({value, onChange, optionList}) => {
-    return (
-        <select className="ControlMenu" value={value} onChange={(e) => onChange(e.target.value)}>
-            {optionList.map((it, idx) => (
-                <option key={idx} value={it.value}>
-                    {it.name}
-                </option>
-            ))} 
-        </select>
-    );
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select
+      className="ControlMenu"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+    >
+      {optionList.map((it, idx) => (
+        <option key={idx} value={it.value}>
+          {it.name}
+        </option>
+      ))}
+    </select>
+  );
 };
 //value -> select의 선택값, onChange -> select가 선택할 때 바꿀 함수, optionList -> 안에 옵션
 
 const DiaryList = ({ diaryList }) => {
-    const navigate = useNavigate();
-    const [sortType, setSortType] = useState("lastest");
-    const [filter, setFilter] = useState("all");
+  const navigate = useNavigate();
+  const [sortType, setSortType] = useState("lastest");
+  const [filter, setFilter] = useState("all");
 
-const getProcessedDiaryList = () => {
+  const getProcessedDiaryList = () => {
+    const filterCallBack = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
 
-    const filterCallBack = (item) =>{
-        if(filter === "good") {
-            return parseInt(item.emotion) <= 3;
-        }else{
-            return parseInt(item.emotion) > 3;
-        }
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return parseInt(b.date) - parseInt(a.date); //parseInt를 하는 이유 : 문자열이 들어올 수 도 있으니까 Int로 다 맞춰주기 위해
+      } else {
+        return parseInt(a.date) - parseInt(b.date);
+      }
+    };
 
-    }
+    const copyList = JSON.parse(JSON.stringify(diaryList)); // diaryList를 문자화 -> parse를 시키면 다시 배열로 돌아와
 
-
-    const compare = (a,b) => {
-        if(sortType === "latest"){
-            return parseInt(b.date) - parseInt(a.date); //parseInt를 하는 이유 : 문자열이 들어올 수 도 있으니까 Int로 다 맞춰주기 위해
-        }else{
-            return parseInt(a.date) - parseInt(b.date);
-        }
-    }
-
-    const copyList = JSON.parse(JSON.stringify(diaryList));// diaryList를 문자화 -> parse를 시키면 다시 배열로 돌아와
-
-    const filteredList = filter === 'all' ? copyList : copyList.filter((it) => filterCallBack(it));
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
     const sortedList = filteredList.sort(compare); //바로 sort하면 원본 배열이 망가지니 copy본에 sort
 
     return sortedList;
-} 
-    
+  };
 
-
-    return(
-        <div className="DiaryList">
-            <div className="menu_wrapper">
-                <div className="left_col">
-                    <ControlMenu 
-                    value={sortType} 
-                    onChange={setSortType}
-                    optionList={sortOptionList}
-                    />
-                    <ControlMenu 
-                    value={filter}
-                    onChange={setFilter}
-                    optionList={filterOptionList}
-                    />
-                </div>
-                <div className="right_col">
-                    <MyButton 
-                        type={'positive'} 
-                        text="새 일기 쓰기" 
-                        onClick={ () => navigate("/new")}
-                        />
-                </div>
-            </div>
-            
-            
-            {getProcessedDiaryList().map((it)=> (
-                <DairyItem key={it.id} {...it}/>
-            ))}
+  return (
+    <div className="DiaryList">
+      <div className="menu_wrapper">
+        <div className="left_col">
+          <ControlMenu
+            value={sortType}
+            onChange={setSortType}
+            optionList={sortOptionList}
+          />
+          <ControlMenu
+            value={filter}
+            onChange={setFilter}
+            optionList={filterOptionList}
+          />
         </div>
-    );
-};
+        <div className="right_col">
+          <MyButton
+            type={"positive"}
+            text="새 일기 쓰기"
+            onClick={() => navigate("/new")}
+          />
+        </div>
+      </div>
 
+      {getProcessedDiaryList().map((it) => (
+        <DairyItem key={it.id} {...it} />
+      ))}
+    </div>
+  );
+};
+DiaryList.defaultProps = {
+  diaryList: [],
+};
 export default DiaryList;
