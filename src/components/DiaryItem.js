b2082c0fa8414b3de44146da207caf1019d9fe36
yujin <miru93198@gmail.com>
2023-01-17 01:42:41 +0900

    new page 일부 제작

--- src/components/DiaryItem.js
+++ src/components/DiaryItem.js
@@ -1,21 +1,29 @@
 import MyButton from "./MyButton";
+import { useNavigate } from "react-router-dom";
 
 const DairyItem = ({ id, emotion, content, date }) => {
+  const navigate = useNavigate();
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || "";
 
   const strDate = new Date(parseInt(date)).toLocaleDateString();
-
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
   return (
     <div className="DiaryItem">
       <div
+        onClick={goDetail}
         className={["emotion_img_wrapper", `/assets/emotion${emotion}`].join(
           " "
         )}
       >
         <img src={process.env.PUBLIC_URL + `/assets/emotion${emotion}.png`} />
       </div>
-      <div className="info_wrapper">
+      <div className="info_wrapper" onClick={goDetail}>
         <div className="diary_date">{strDate}</div>
         <div className="diary_content_preview">
           {content.slice(0, 25)}
@@ -23,7 +31,7 @@ const DairyItem = ({ id, emotion, content, date }) => {
         </div>
       </div>
       <div className="btn_wrapper">
-        <MyButton text={"수정하기"} />
+        <MyButton text={"수정하기"} onClick={goEdit} />
       </div>
     </div>
   );
--- src/pages/New.js
+++ src/pages/New.js
@@ -1,10 +1,43 @@
+import { useNavigate } from "react-router-dom";
+
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import { useState } from "react";
+
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10); //ISO형식의 문자열 반환 YYYY-MM-DD형식으로 9번째 자리까지만 자름
+};
+
 const New = () => {
-    return(
-        <div>
-        <h1>New</h1>
-        <p>이 곳은 일기 작성 페이지입니다.</p>
+  console.log();
+  const navigate = useNavigate();
+  const [date, setDate] = useState(getStringDate(new Date())); //getStringDate(new Date()) 로 초기값을 해주어야 현재 내 날짜로 초기값됨
+  return (
+    <div>
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={
+          <MyButton text={" < 뒤로가기 "} onClick={() => navigate(-1)} />
+        }
+      />
+
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              value={date}
+              onChange={(e) => {
+                setDate(e.target.value);
+              }}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
     </div>
-    );
+  );
 };
 
 export default New;
